345c423cbfc9e1f18b2ddeaca4ddcbab117998da
macolko <maciej.olkO@braze.com>
2021-10-12 16:26:35 -0400

    Add package script

---
+++ package.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+version="0.1.3"
+
+echo "Creating build directory"
+
+if [ -d "./build" ]
+then 
+    echo "Build directory exists, skipping"
+else
+    mkdir test
+fi
+
+echo "Packaging depencies"
+cd braze_user_csv_import
+pip install --target ./package requests
+echo "Packaging the app"
+cd package
+zip -r ../braze-lambda-user-csv-import-v"$version".zip .
+cd ..
+zip -g braze-lambda-user-csv-import-v"$version".zip app.py
+mv braze-lambda-user-csv-import-v"$version".zip ../build
+rm -r package
+echo "Done"
