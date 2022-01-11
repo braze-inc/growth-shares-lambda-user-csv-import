6983ee533f2ee17be34d88d46af026637fce6432
macolko <maciej.olkO@braze.com>
2022-01-11 13:05:39 -0500

    style: single quote to double quotes in tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from braze_user_csv_import import app
 
 
 def test_lambda_handler_fails_assert_event_logged(mocker, capsys):
-    headers = ['header1', 'header2']
+    headers = ["header1", "header2"]
     offset = 7256
     event = {"Records": [
         {"s3": {"bucket": {"name": "test"}, "object": {"key": "test"}}}]}
@@ -15,7 +15,7 @@ def test_lambda_handler_fails_assert_event_logged(mocker, capsys):
                                       processed_users=999)
     # Set off fatal exception during file processing
     mock_processor.process_file.side_effect = app.FatalAPIError("Test error")
-    mock_processor = mocker.patch('braze_user_csv_import.app.CsvProcessor',
+    mock_processor = mocker.patch("braze_user_csv_import.app.CsvProcessor",
                                   return_value=mock_processor)
     with pytest.raises(Exception):
         app.lambda_handler(event, None)
@@ -28,7 +28,7 @@ def test_lambda_handler_fails_assert_event_logged(mocker, capsys):
 
 
 def test_successful_import_offset_progresses(mocker, users, csv_processor):
-    mocker.patch('braze_user_csv_import.app._post_users', return_value=75)
+    mocker.patch("braze_user_csv_import.app._post_users", return_value=75)
     chunks = [users] * 5
     csv_processor.processing_offset = 100
 
@@ -38,7 +38,7 @@ def test_successful_import_offset_progresses(mocker, users, csv_processor):
 
 
 def test_failed_import_offset_does_not_progress(mocker, users, csv_processor):
-    mocker.patch('braze_user_csv_import.app._post_users',
+    mocker.patch("braze_user_csv_import.app._post_users",
                  side_effect=RuntimeError)
     chunks = [users] * 5
     csv_processor.processing_offset = 100
@@ -50,36 +50,36 @@ def test_failed_import_offset_does_not_progress(mocker, users, csv_processor):
 
 
 def test__process_row_empty_string_should_ignore():
-    row = {'external_id': 'user1', 'attribute1': '', 'attribute2': 'value'}
-    processed_row = app._process_row(row)
+    row = {"external_id": "user1", "attribute1": "", "attribute2": "value"}
+    processed_row = app._process_row(row, {})
     assert len(processed_row) == 2
-    assert 'attribute1' not in processed_row
+    assert "attribute1" not in processed_row
 
 
 def test__process_row_null_string_should_convert_to_none():
-    row = {'external_id': 'user1', 'attribute1': 'null'}
-    processed_row = app._process_row(row)
+    row = {"external_id": "user1", "attribute1": "null"}
+    processed_row = app._process_row(row, {})
     assert len(processed_row) == 2
-    assert processed_row['attribute1'] == None
+    assert processed_row["attribute1"] == None
 
 
 def test__process_row_single_digit_value():
-    row = {'external_id': '0166ecc9-asd9-0305-sjn9-efd44fe61b96', 'attribute': '0'}
-    processed_row = app._process_row(row)
+    row = {"external_id": "0166ecc9-asd9-0305-sjn9-efd44fe61b96", "attribute": "0"}
+    processed_row = app._process_row(row, {})
     assert len(processed_row) > 1
 
 
 def test__process_list_string_should_deconstruct():
-    row = {'external_id': 'user1', 'attribute1': "['value1', 'value2']"}
-    processed_row = app._process_row(row)
+    row = {"external_id": "user1", "attribute1": "['value1', 'value2']"}
+    processed_row = app._process_row(row, {})
     assert len(processed_row) == 2
-    assert isinstance(processed_row['attribute1'], list)
+    assert isinstance(processed_row["attribute1"], list)
 
 
 def test__post_to_braze_api_retry_error_assert_fn_retried(mocker, users):
     app._post_to_braze.retry.sleep = mocker.Mock()
-    mocker.patch('requests.post')
-    handler_mock = mocker.patch('braze_user_csv_import.app._handle_braze_response',
+    mocker.patch("requests.post")
+    handler_mock = mocker.patch("braze_user_csv_import.app._handle_braze_response",
                                 side_effect=app.APIRetryError)
 
     with pytest.raises(Exception):
@@ -89,7 +89,7 @@ def test__post_to_braze_api_retry_error_assert_fn_retried(mocker, users):
 
 def test__post_to_braze_retry_connection_error_assert_fn_retried(mocker, users):
     app._post_to_braze.retry.sleep = mocker.Mock()
-    request_mock = mocker.patch('requests.post', side_effect=RequestException)
+    request_mock = mocker.patch("requests.post", side_effect=RequestException)
 
     with pytest.raises(Exception):
         app._post_to_braze(users)
@@ -97,8 +97,8 @@ def test__post_to_braze_retry_connection_error_assert_fn_retried(mocker, users):
 
 
 def test__post_to_braze_fatal_exception_not_retried(mocker, users):
-    mocker.patch('requests.post')
-    handler_mock = mocker.patch('braze_user_csv_import.app._handle_braze_response',
+    mocker.patch("requests.post")
+    handler_mock = mocker.patch("braze_user_csv_import.app._handle_braze_response",
                                 side_effect=app.FatalAPIError)
 
     with pytest.raises(Exception):
@@ -107,7 +107,7 @@ def test__post_to_braze_fatal_exception_not_retried(mocker, users):
 
 
 def test__handle_braze_response_success(mocker):
-    mocker.patch('json.loads', return_value={"message": "success"})
+    mocker.patch("json.loads", return_value={"message": "success"})
     res = mocker.Mock(status_code=201)
 
     error_users = app._handle_braze_response(res)
@@ -116,7 +116,7 @@ def test__handle_braze_response_success(mocker):
 
 def test__handle_braze_response_some_processed(mocker):
     res = mocker.Mock(status_code=201)
-    mocker.patch('json.loads', return_value={
+    mocker.patch("json.loads", return_value={
                  "errors": [{"there were some errors with index 1"}]})
 
     error_users = app._handle_braze_response(res)
@@ -125,7 +125,7 @@ def test__handle_braze_response_some_processed(mocker):
 
 def test__handle_braze_response_server_error_max_retries_not_reached_raises_non_fatal_api_error(mocker):
     res = mocker.Mock(status_code=429)
-    mocker.patch('json.loads', return_value={"errors": {"too many requests"}})
+    mocker.patch("json.loads", return_value={"errors": {"too many requests"}})
 
     with pytest.raises(app.APIRetryError):
         app._handle_braze_response(res)
@@ -133,7 +133,7 @@ def test__handle_braze_response_server_error_max_retries_not_reached_raises_non_
 
 def test__handle_braze_response_authorization_failure_raises_fatal_error(mocker):
     res = mocker.Mock(status_code=405)
-    mocker.patch('json.loads', return_value={
+    mocker.patch("json.loads", return_value={
         "errors": {"some server errors"}})
 
     with pytest.raises(app.FatalAPIError):
